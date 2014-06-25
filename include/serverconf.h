02c0c6cc9dc3c9224dda9ac06063eba5d149c5b4
Takeo Van Miyamotsuraw <miyamofigol@gmail.com>
2014-06-26 01:18:49 +0900

    header files in include directory are added.

---
+++ include/serverconf.h
@@ -0,0 +1,6 @@
+#ifndef __SERVER_CONF__
+#define __SERVER_CONF__
+
+#define HTTP_PORT 80
+
+#endif
---
+++ include/serversoc.h
@@ -0,0 +1,16 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef __SERVER_SOC__
+#define __SERVER_SOC__
+
+int prepare_socket(int);
+void accept_loop(int);
+
+#endif
