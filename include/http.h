8ad1f65b202378b954bd3f828a062a20b222fda0
Takeo Miyamoto <miyamofigo@gmail.com>
2014-07-07 23:52:17 +0900

    added send_message function and described the routine of parse_message function.

--- include/http.h
+++ include/http.h
@@ -1,3 +1,7 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +9,7 @@
 #ifndef __HTTP_SERVER_DEF__
 #define __HTTP_SERVER_DEF__
 
+#define SEND_BUF_SIZE 512
 #define RECV_BUF_SIZE 512
 #define METHOD_NAME_LENGTH 16
 #define URI_ADDRESS_SIZE 256
@@ -15,10 +20,10 @@ typedef struct _req_header {
   char path[URI_ADDRESS_SIZE];
   char version[HTTP_VERSION_SIZE];
 } req_header; 
+
 void http_server_routine(int);
 typedef req_header* (*req_handler_t)(req_header*, char*);
 req_header* parse_message(req_handler_t, char*); 
 req_header* justscan(req_header*, char*);
-
 #endif
 
